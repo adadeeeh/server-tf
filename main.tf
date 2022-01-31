22f43832b28f2dfc34126fcefab8cbeb9f27b090
Faturrahman Ma'ruf <pedro@MacBook-Air-M1.local>
2022-01-31 12:21:48 +0700

    change sg

--- main.tf
+++ main.tf
@@ -70,7 +70,7 @@ resource "aws_instance" "app2" {
   instance_type = var.instance_type
 
   subnet_id              = data.terraform_remote_state.network.outputs.private_subnet[0]
-  vpc_security_group_ids = data.terraform_remote_state.network.outputs.sg_web
+  vpc_security_group_ids = data.terraform_remote_state.network.outputs.sg_web_public
 
   user_data = <<-EOF
     #!/bin/bash
